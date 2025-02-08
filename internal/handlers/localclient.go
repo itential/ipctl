abed428a01ccd51d7d755ffbeb64a2df351a07ca
Peter Sprygada <privateip@users.noreply.github.com>
2025-02-08 17:32:20 -0500

    update source code licenses (#17)
    This commit is a result from running `make licenses` to update all
    license headers in source code files.   It also updates `Makefile` to
    include comments for each target.

--- internal/handlers/localclient.go
+++ internal/handlers/localclient.go
@@ -1,3 +1,7 @@
+// Copyright 2024 Itential Inc. All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
 package handlers
 
 import (
