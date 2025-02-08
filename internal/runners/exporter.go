07ce093c64d6965bd8568c4c5599d1eea4d1bb90
Peter Sprygada <privateip@users.noreply.github.com>
2025-02-08 17:32:32 -0500

    adds additional command support for transformations (#18)
    This commit adds additional command support when working with
    transformations.  The added commands in this commit include:
    
    - `import transformation ...`
    - `export transformation ...`
    - `push transformation ...`
    - `pull transformation ...`

---
+++ internal/runners/exporter.go
@@ -0,0 +1,44 @@
+package runners
+
+import (
+	"github.com/itential/ipctl/internal/flags"
+	"github.com/itential/ipctl/internal/utils"
+	"github.com/itential/ipctl/pkg/logger"
+)
+
+const (
+	exportSuccessMessage = "Successfully exported %s `%s` to `%s`"
+)
+
+type ExportAction struct {
+	Filename string
+	Data     any
+	Common   *flags.AssetExportCommon
+}
+
+type ExportActionResponse struct {
+	Filename string
+	Message  string
+}
+
+func NewExportAction(data any, fn string, common *flags.AssetExportCommon) ExportAction {
+	logger.Trace()
+	return ExportAction{
+		Data:     data,
+		Filename: fn,
+		Common:   common,
+	}
+}
+
+func (a ExportAction) Do() error {
+	logger.Trace()
+
+	var common flags.AssetExportCommon
+	utils.LoadObject(a.Common, &common)
+
+	if err := utils.WriteJsonToDisk(a.Data, a.Filename, common.Path); err != nil {
+		return err
+	}
+
+	return nil
+}
