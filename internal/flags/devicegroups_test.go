d8592b39965a3832f6d16d2dd42f49b8a0a52fa2
Peter Sprygada <peter.sprygada@itential.com>
2025-04-09 12:25:01 -0400

    Adds missing unit tests for flags package
    There were a few flags structs missing unit tests which are fixed in
    this change.

---
+++ internal/flags/devicegroups_test.go
@@ -0,0 +1,11 @@
+// Copyright 2024 Itential Inc. All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
+package flags
+
+import "testing"
+
+func TestDeviceGroupCreateOptions(t *testing.T) {
+	checkFlags(t, &DeviceGroupCreateOptions{}, []string{"description"})
+}
