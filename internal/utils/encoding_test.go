de5d8a7272d319356f56a4900d66236be654a083
Peter Sprygada <privateip@users.noreply.github.com>
2025-04-09 12:08:10 -0400

    Add unit tests to package utils (#116)
    A continuing initaitive is to add unit test cases to the application to
    test individual functions and code blocks.  This addes unit test cases
    to the utils package and removes old code that is no longer used in the
    application.

---
+++ internal/utils/encoding_test.go
@@ -0,0 +1,63 @@
+// Copyright 2024 Itential Inc. All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// SampleStruct is a sample data structure for testing.
+type SampleStruct struct {
+	Name  string `json:"name" yaml:"name"`
+	Value int    `json:"value" yaml:"value"`
+}
+
+// TestToMapSuccess tests the successful conversion of a struct to a map.
+func TestToMapSuccess(t *testing.T) {
+	input := SampleStruct{
+		Name:  "test",
+		Value: 42,
+	}
+
+	var output map[string]interface{}
+	err := ToMap(input, &output)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "test", output["name"])
+	assert.Equal(t, float64(42), output["value"]) // JSON unmarshals numbers into float64
+}
+
+// TestToMapInvalidInput tests ToMap with an invalid input (channel can't be marshaled).
+func TestToMapInvalidInput(t *testing.T) {
+	ch := make(chan int)
+	var output map[string]interface{}
+	err := ToMap(ch, &output)
+
+	assert.Error(t, err)
+}
+
+// TestUnmarshalDataJSON tests successful JSON unmarshalling.
+func TestUnmarshalDataJSON(t *testing.T) {
+	data := []byte(`{"name": "json", "value": 123}`)
+	var obj SampleStruct
+
+	UnmarshalData(data, &obj)
+
+	assert.Equal(t, "json", obj.Name)
+	assert.Equal(t, 123, obj.Value)
+}
+
+// TestUnmarshalDataYAML tests fallback to YAML unmarshalling.
+func TestUnmarshalDataYAML(t *testing.T) {
+	data := []byte("name: yaml\nvalue: 456")
+	var obj SampleStruct
+
+	UnmarshalData(data, &obj)
+
+	assert.Equal(t, "yaml", obj.Name)
+	assert.Equal(t, 456, obj.Value)
+}
