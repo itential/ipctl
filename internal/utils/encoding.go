20c55f2ca46ec5368dc78af5d4c66db929cc07c0
Peter Sprygada <peter.sprygada@itential.com>
2025-04-09 11:56:57 -0400

    Add unit tests to package utils
    A continuing initaitive is to add unit test cases to the application to
    test individual functions and code blocks.  This addes unit test cases
    to the utils package and removes old code that is no longer used in the
    application.

--- internal/utils/encoding.go
+++ internal/utils/encoding.go
@@ -11,6 +11,9 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// ToMap accepts any object and will return at as a map using json marshal and
+// unmarshal.  This fuction will return an error if if fails to marshal or
+// unmarshal the input object.
 func ToMap(in any, out any) error {
 	b, err := json.Marshal(in)
 	if err != nil {
@@ -23,6 +26,8 @@ func ToMap(in any, out any) error {
 // will first attempt to unmarshal the byte array as JSON.  If that fails, it
 // will attempt to unmarshal the data as YAML.
 func UnmarshalData(data []byte, ptr any) {
+	// FIXME (privateip) This function should be refactored to return an error
+	// instead of simply logging a fatal error.
 	if err := json.Unmarshal(data, ptr); err != nil {
 		logger.Error(err, "failed to unmarshal json data")
 		if err = yaml.Unmarshal(data, ptr); err != nil {
