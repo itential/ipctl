22d49b314dd87f42ae165cd707c8366735e9245c
Peter Sprygada <privateip@users.noreply.github.com>
2025-04-10 11:18:08 -0400

    Adds missing unit tests for flags package (#117)
    There were a few flags structs missing unit tests which are fixed in
    this change.

--- internal/flags/models_test.go
+++ internal/flags/models_test.go
@@ -9,3 +9,15 @@ import "testing"
 func TestModelCreateOptions(t *testing.T) {
 	checkFlags(t, &ModelCreateOptions{}, []string{"description", "schema", "replace"})
 }
+
+func TestModelDeleteOptions(t *testing.T) {
+	checkFlags(t, &ModelDeleteOptions{}, []string{"all", "delete-instances"})
+}
+
+func TestModelExportOptions(t *testing.T) {
+	checkFlags(t, &ModelExportOptions{}, []string{"expand"})
+}
+
+func TestModelImportOptions(t *testing.T) {
+	checkFlags(t, &ModelImportOptions{}, []string{"all", "skip-checks"})
+}
